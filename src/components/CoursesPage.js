beb1133ed801684c9211b057549dcd6bad403c69
Richard Yvarra <richard.yvarra@ibm.com>
2022-01-24 17:31:14 -0500

    useState in use

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
+function CoursesPage () {
+    const [ courses, setCourses ] = useState([])
 
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
-
-  render() {
+    useEffect(() => {
+        getCourses().then(_courses => setCourses(_courses));
+      },[]);
+    
+ 
+  
     return (
       <>
         <h2>Courses</h2>
@@ -23,7 +22,7 @@ class CoursesPage extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.courses.map(course => {
+            {courses.map(course => {
               return (
                 <tr key={course.id}>
                   <td>{course.title}</td>
@@ -37,6 +36,6 @@ class CoursesPage extends React.Component {
       </>
     );
   }
-}
+
 
 export default CoursesPage;
